2ca09c76ba426ecf2f93903a7cfdc7bd707cff8c
Ming <54872601+1998code@users.noreply.github.com>
2021-01-06 13:22:53 +0800

    Update articles.js

--- src/components/sections/articles.js
+++ src/components/sections/articles.js
@@ -38,8 +38,9 @@ const StyledContentWrapper = styled(ContentWrapper)`
     .articles {
       display: flex;
       justify-content: flex-start;
-      overflow-x: scroll;
-      overflow-y: hidden;
+      overflow: hidden;
+      /*overflow-x: scroll;
+      overflow-y: hidden;*/
       -webkit-overflow-scrolling: touch;
       margin: -2rem 0 0 0;
       padding: 0 2rem;
