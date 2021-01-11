6b7fcd9ca148d5e524e46de39679cbfac3a9a1f5
Ming <54872601+1998code@users.noreply.github.com>
2021-01-11 09:27:43 +0800

    Update contact.js

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -44,7 +44,7 @@ const StyledContentWrapper = styled(ContentWrapper)`
       .avatar {
         width: 100%;
         max-width: 8.75rem;
-        border-radius: 50%;
+        border-radius: 85%;
         margin-right: 4rem;
         margin-bottom: 2rem;
         @media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
