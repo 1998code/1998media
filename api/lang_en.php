4eea2757ba00195894b3c572d43076491689b1a2
1998Code <54872601+1998code@users.noreply.github.com>
2021-06-03 17:29:10 +0800

    Update

--- api/lang_en.php
+++ api/lang_en.php
@@ -7,3 +7,10 @@ define("_Designer", "Designer");
 define("_App", "App");
 define("_Developer", "Developer");
 define("_Scholar", "Scholar");
+
+define("_English", "English");
+define("_Chinese", "Chinese");
+define("_Korean", "Korean");
+define("_Japanese", "Japanese");
+
+define("_About", "About");
