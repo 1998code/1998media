87e0845ed504465b39e6763579772d98b0e885c6
1998Code <54872601+1998code@users.noreply.github.com>
2021-06-03 14:28:37 +0800

    Update

---
+++ api/lang_en.php
@@ -0,0 +1,8 @@
+<?php
+
+define("_REGISTER", "Register");
+define("_NAME", "Name");
+define("_USERNAME", "Username");
+define("_EMAIL", "Email");
+define("_ADDRESS", "Address");
+define("_SUBMIT", "Submit");
