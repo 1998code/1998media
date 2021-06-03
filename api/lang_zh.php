e23b71fd0af42ad7ba26bad80c15e205db93b11d
1998Code <54872601+1998code@users.noreply.github.com>
2021-06-03 17:17:37 +0800

    Update lang_zh.php

--- api/lang_zh.php
+++ api/lang_zh.php
@@ -1,9 +1,9 @@
 <?php
-define("_Hello", "Hello");
-define("_Im", "I'm");
+define("_Hello", "你好");
+define("_Im", "我是");
 define("_MING", "銘");
-define("_UI", "UI");
-define("_Designer", "Designer");
-define("_App", "App");
-define("_Developer", "Developer");
-define("_Scholar", "Scholar");
+define("_UI", "用戶界面");
+define("_Designer", "設計師");
+define("_App", "應用");
+define("_Developer", "開發者");
+define("_Scholar", "優勝者");
