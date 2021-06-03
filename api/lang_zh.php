92442145bd262f582f80a1000943fab77086c720
1998Code <54872601+1998code@users.noreply.github.com>
2021-06-03 15:24:47 +0800

    Create lang_zh.php

---
+++ api/lang_zh.php
@@ -0,0 +1,8 @@
+<?php
+
+define("_REGISTER", "登記");
+define("_NAME", "Name");
+define("_USERNAME", "Username");
+define("_EMAIL", "Email");
+define("_ADDRESS", "Address");
+define("_SUBMIT", "Submit");
