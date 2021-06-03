fd58ed155a0ab29d692273f3c3ad87100092a85c
1998Code <54872601+1998code@users.noreply.github.com>
2021-06-03 14:45:37 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,49 @@
 <?php
+session_start();
 
-header('content-type: application/json');
-echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
+// Set Language variable
+if(isset($_GET['lang']) && !empty($_GET['lang'])){
+    $_SESSION['lang'] = $_GET['lang'];
+   
+    if(isset($_SESSION['lang']) && $_SESSION['lang'] != $_GET['lang']){
+     echo "<script type='text/javascript'> location.reload(); </script>";
+    }
+   }
+   
+   // Include Language file
+   if(isset($_SESSION['lang'])){
+    include "lang_".$_SESSION['lang'].".php";
+   }else{
+    include "lang_en.php";
+   }
+   ?>
+   
+   <!doctype html>
+   <html>
+    <meta content='text/html; charset=UTF-8' http-equiv='Content-Type'/>
+    <body >
+    <script>
+    function changeLang(){
+     document.getElementById('form_lang').submit();
+    }
+    </script>
+   
+    <!-- Language -->
+    <form method='get' action='' id='form_lang' >
+      Select Language : <select name='lang' onchange='changeLang();' >
+      <option value='en' <?php if(isset($_SESSION['lang']) && $_SESSION['lang'] == 'en'){ echo "selected"; } ?> >English</option>
+      <option value='pl' <?php if(isset($_SESSION['lang']) && $_SESSION['lang'] == 'pl'){ echo "selected"; } ?> >Polish</option>
+     </select>
+    </form>
+   
+   <!-- Form -->
+    <h1><?= _REGISTER ?></h1>
+    <form method='post' action=''>
+     <?= _NAME ?> : <input type='text' name='name' /><br>
+     <?= _USERNAME ?> : <input type='text' name='username' /><br>
+     <?= _EMAIL ?> : <input type='text' name='email' /><br>
+     <?= _ADDRESS ?> : <input type='text' name='address' /><br>
+     <input type='submit' value='<?= _SUBMIT ?>'>
+    </form>
+    </body>
+   </html>
