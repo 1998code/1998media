b9d0440489e52966af6eaec5596e52530ac43f06
1998Code <54872601+1998code@users.noreply.github.com>
2021-06-03 16:45:28 +0800

    Update

--- api/lang_en.php
+++ api/lang_en.php
@@ -1,8 +1,3 @@
 <?php
 
-define("_REGISTER", "Register");
-define("_NAME", "Name");
-define("_USERNAME", "Username");
-define("_EMAIL", "Email");
-define("_ADDRESS", "Address");
-define("_SUBMIT", "Submit");
+define("_MING", "MING");
--- api/lang_zh.php
+++ api/lang_zh.php
@@ -1,8 +1,3 @@
 <?php
 
-define("_REGISTER", "登記");
-define("_NAME", "Name");
-define("_USERNAME", "Username");
-define("_EMAIL", "Email");
-define("_ADDRESS", "Address");
-define("_SUBMIT", "Submit");
+define("_MING", "銘");
