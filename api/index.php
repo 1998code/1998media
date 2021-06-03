ded639663c26ffe29425a583bf3107572fcd5c59
1998Code <54872601+1998code@users.noreply.github.com>
2021-06-03 22:01:12 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -3,19 +3,19 @@ session_start();
 
 // Set Language variable
 if(isset($_GET['lang']) && !empty($_GET['lang'])){
-    $_SESSION['lang'] = $_GET['lang'];
-   
-    if(isset($_SESSION['lang']) && $_SESSION['lang'] != $_GET['lang']){
-     echo "<script type='text/javascript'> location.reload(); </script>";
-    }
-   }
-   
-   // Include Language file
-   if(isset($_SESSION['lang'])){
-    include "lang_".$_SESSION['lang'].".php";
-   }else{
-    include "lang_en.php";
-   }
+ $_SESSION['lang'] = $_GET['lang'];
+
+ if(isset($_SESSION['lang']) && $_SESSION['lang'] != $_GET['lang']){
+  echo "<script type='text/javascript'> location.reload(); </script>";
+ }
+}
+
+// Include Language file
+if(isset($_SESSION['lang'])){
+ include "lang_".$_SESSION['lang'].".php";
+}else{
+ include "lang_en.php";
+}
 ?>
 
 <!DOCTYPE html>
