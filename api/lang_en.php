b297727570a8d0337d7c904bb9afc885691416a8
1998Code <54872601+1998code@users.noreply.github.com>
2021-06-07 12:20:55 +0800

    Update

--- api/lang_en.php
+++ api/lang_en.php
@@ -2,10 +2,8 @@
 define("_Hello", "Hello");
 define("_Im", "I'm");
 define("_MING", "MING");
-define("_UI", "UI");
-define("_Designer", "Designer");
-define("_App", "App");
-define("_Developer", "Developer");
+define("_UIDesigner", "UI Designer");
+define("_AppDeveloper", "App Developer");
 define("_Scholar", "Scholar");
 define("_English", "English");
 define("_Chinese", "Chinese");
--- api/lang_zh.php
+++ api/lang_zh.php
@@ -2,10 +2,8 @@
 define("_Hello", "你好");
 define("_Im", "我是");
 define("_MING", "銘");
-define("_UI", "用戶界面");
-define("_Designer", "設計師");
-define("_App", "應用");
-define("_Developer", "開發者");
+define("_UIDeveloper", "用戶界面設計師");
+define("_AppDeveloper", "應用開發者");
 define("_Scholar", "優勝者");
 define("_English", "英文");
 define("_Chinese", "中文");
