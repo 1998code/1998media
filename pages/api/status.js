9c1d0d05ceed0097a1f9c11d994d67d4f073a0cf
Ming <54872601+1998code@users.noreply.github.com>
2022-02-20 12:52:45 +0800

    Update api

--- pages/api/status.js
+++ pages/api/status.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'Ming' })
+  res.status(200).json({ status: '200', message: 'OK', system: 'Operational' });
 }
