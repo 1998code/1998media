4d9d29ffbf868aacea70151c80ab2cc70832baac
Ming <54872601+1998code@users.noreply.github.com>
2022-02-22 00:07:33 +0800

    Update

--- pages/api/dlRYD.js
+++ pages/api/dlRYD.js
@@ -5,14 +5,14 @@ import { promisify } from 'util';
 import fetch from 'node-fetch';
 
 const pipeline = promisify(stream.pipeline);
-const url = 'https://1998.media/s5/RYD.tar.gz';
+const url = 'https://1998.media/s5/RYDmac.tar.gz';
 
 const handler = async (req, res) => {
   const response = await fetch(url);
   if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
 
   res.setHeader('Content-Type', 'application/tar+gzip');
-  res.setHeader('Content-Disposition', 'attachment; filename=RYD.tar.gz');
+  res.setHeader('Content-Disposition', 'attachment; filename=RYDmac.tar.gz');
   await pipeline(response.body, res);
 };
 
