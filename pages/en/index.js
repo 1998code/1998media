eb47333db21ba8e2325bb6acc84695f4e8a80f84
1998code <54872601+1998code@users.noreply.github.com>
2022-08-15 00:12:59 +0800

    add fontawesome

--- pages/en/index.js
+++ pages/en/index.js
@@ -19,6 +19,7 @@ export default function Home() {
         <title>1998 MEDIA (Official Website)</title>
         <meta name="description" content="The Official Website of 1998 MEDIA." />
         <link rel="icon" href="/favicon.png" />
+        <link rel="stylesheet" href="https://cdn.1998.media/css/fontawesome.css" />
       </Head>
       <main>
         <Header />
