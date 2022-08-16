bb8b23d61eaab8529c445b8b1bb42a416ac7b188
1998code <54872601+1998code@users.noreply.github.com>
2022-08-16 16:30:53 +0800

    Update newrelic.js

--- pages/api/newrelic.js
+++ pages/api/newrelic.js
@@ -2,5 +2,5 @@ import newrelic from 'newrelic';
 
 export default function (req, res) {
     newrelic;
-    res.status(200).json({say: 'Hello World!'})
+    res.status(200).json({say: 'Hello World!', hireMe: 'job@1998.media'});
 }
