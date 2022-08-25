43fba22ea1596c880ba1de6aa2adceaaed739b3c
1998code <54872601+1998code@users.noreply.github.com>
2022-08-25 09:45:42 +0800

    Create bootstrap.js

---
+++ pages/en/bootstrap.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Bootstrap() {
+  return (
+    <div>
+      <Head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+        <title>Themes by 1998Code</title>
+        <meta name="description"
+            content="Free themes for students, teachers, and public use in learning purpose. Made with Bootstrap 5." />
+      </Head>
+      <main>
+        <iframe src="https://themes.1998.media/" frameborder="0" width="100%" height="100%" class="fixed"></iframe>
+      </main>
+      
+    </div>
+  )
+}
