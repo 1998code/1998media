0e636e1d4d4bc1c9baf36e9f7f09d8b4c2f98152
1998code <54872601+1998code@users.noreply.github.com>
2022-09-06 19:53:07 +0800

    Create index.js

---
+++ pages/ru/index.js
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+
+export default function Home() {
+  fetch ('/api/newrelic')
+    .then (res => res.json())
+    .then (data => {
+      // console.log(data)
+    }).catch (err => {
+      console.log(err)
+    }
+  )
+  return (
+    <div>
+      <Head>
+        <title>1998 MEDIA (Официальный сайт)</title>
+        <meta name="description" content="1998 MEDIA Единственный официальный сайт." />
+        <link rel="icon" href="/favicon.png" />
+        <link rel="stylesheet" href="https://cdn.1998.media/css/sfprodisplay.css" />
+        <link rel="stylesheet" href="https://cdn.1998.media/css/fontawesome.css" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1941913120815371"></script>
+      </Head>
+      <main className="select-none overflow-hidden w-screen h-screen flex justify-center items-center">
+        <div class="text-center">
+          <h1 className="text-black text-4xl">Этот сайт еще не доступен на вашем языке, вы хотите перенаправить на английский язык?</h1>
+          <br/ >
+          <a href="/en" className="text-black text-5xl text-center">
+            <i className="fas fa-globe-asia mr-3"></i>
+            хорошо, прыгай сейчас
+            <i className="fas fa-rotate-left ml-3"></i>
+          </a>
+        </div>
+      </main>
+    </div>
+  )
+}
