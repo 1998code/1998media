277fb6552d9087fadd7a8d53506ced59c11787e5
1998code <54872601+1998code@users.noreply.github.com>
2022-09-06 19:50:04 +0800

    Update redirect

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,16 @@ export const config = {
 }
 
 export default function middleware(req: NextRequest) {
-  const locale = req.headers.get('accept-language')?.split(',')?.[0] == 'zh-TW' ? 'zh' : 'en'
-  req.nextUrl.pathname = `/${locale}`
-  return NextResponse.rewrite(req.nextUrl)
+  const locale = req.headers.get('accept-language')?.split(',')?.[0]
+  switch (locale) {
+    case 'zh-TW':
+      req.nextUrl.pathname = '/zh'
+      return NextResponse.rewrite(req.nextUrl)
+    case 'zh-CN':
+      req.nextUrl.pathname = '/cn'
+      return NextResponse.rewrite(req.nextUrl)
+    default:
+      req.nextUrl.pathname = '/en'
+      return NextResponse.rewrite(req.nextUrl)
+  }
 }
