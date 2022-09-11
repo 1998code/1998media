b920a045641ea75bea542ef6736756fec59cb2a8
1998code <54872601+1998code@users.noreply.github.com>
2022-09-11 23:10:33 +0800

    Update index.js

--- pages/en/index.js
+++ pages/en/index.js
@@ -27,6 +27,8 @@ export default function Home() {
         <title>1998 MEDIA (Official Website)</title>
         <meta name="description" content="The Official Website of 1998 MEDIA." />
         <link rel="icon" href="/favicon.png" />
+        <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff6eb" />
+        <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000914" />
         <link rel="stylesheet" href="https://cdn.1998.media/css/sfprodisplay.css" />
         <link rel="stylesheet" href="https://cdn.1998.media/css/fontawesome.css" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1941913120815371"></script>
