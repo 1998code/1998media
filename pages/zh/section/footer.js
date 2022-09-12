ba2b2b0083ad8af87578086d1d2db0c1d5b8fbea
1998code <54872601+1998code@users.noreply.github.com>
2022-09-12 16:22:07 +0800

    Update

--- pages/zh/section/footer.js
+++ pages/zh/section/footer.js
@@ -52,7 +52,7 @@ export default function Footer() {
           <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
             {navigation.main.map((item) => (
               <div key={item.name} className="px-5 py-2">
-                <a href={item.href} target="_blank" className="text-base text-gray-500 hover:text-gray-900">
+                <a href={item.href} target="_blank" className="text-base text-gray-500 hover:text-gray-600">
                   {item.name}
                   {item.href.includes('http') ? (
                     <i className="ml-1 fa fa-external-link fa-sm"></i>
