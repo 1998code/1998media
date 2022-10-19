b30ce53776674bb48e018bba2f9f993a9aab4835
1998code <54872601+1998code@users.noreply.github.com>
2022-10-19 21:20:17 +0800

    Update

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,40 @@
+import { useEffect } from 'react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+
+    document.addEventListener('contextmenu', (e) => {
+      e.preventDefault();
+    });
+
+    console.log("Glad that you found here. Are you looking for job? Contact hire+web@1998.media");
+
+    const year = new Date().getFullYear() - 2000 + 2;
+
+    console.log(`
+           #
+          # #
+        # # # #
+      # # # # # #
+    # # # Y${ year } # # #
+      # # # # # #
+        # # # #
+          # #
+           #
+           *
+           *
+           * 
+           * *
+           * * *
+           * *
+           * 
+           
+    `);
+
+  }, []);
+
   return <Component {...pageProps} />
 }
 
