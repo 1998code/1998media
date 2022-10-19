72103588de2a3a39ac01a4044731d086bd1cd53a
1998code <54872601+1998code@users.noreply.github.com>
2022-10-19 20:56:46 +0800

    Update Ranking

--- pages/en/section/achievements.js
+++ pages/en/section/achievements.js
@@ -16,25 +16,35 @@ export default function Achievements() {
         <div className="relative">
           <div className="relative max-w-7xl mx-auto">
             <div className="max-w-7xl mx-auto">
-              <dl className="rounded-lg bg-white dark:bg-black shadow-lg sm:grid sm:grid-cols-4">
-                <div className="flex flex-col border-b border-gray-100 dark:border-gray-900 p-6 text-center sm:border-0 sm:border-r">
+              <dl className="rounded-lg bg-white dark:bg-black shadow-lg sm:grid sm:grid-cols-3 divide-gray-100 dark:divide-gray-900 divide-x-2 divide-y-2">
+                <div className="flex flex-col p-6 text-center">
+                  <dt className="order-3 mt-1 text-lg leading-6 font-medium text-gray-400">2022</dt>
+                  <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">Developer Tools in Kuwait</dt>
+                  <dd className="order-1 text-5xl font-extrabold text-pink-600">#1</dd>
+                </div>
+                <div className="flex flex-col p-6 text-center !border-t-0">
+                  <dt className="order-3 mt-1 text-lg leading-6 font-medium text-gray-400">2022</dt>
+                  <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">Developer Tools in Taiwan</dt>
+                  <dd className="order-1 text-5xl font-extrabold text-green-600">#1</dd>
+                </div>
+                <div className="flex flex-col p-6 text-center !border-t-0">
                   <dt className="order-3 mt-1 text-lg leading-6 font-medium text-gray-400">2022</dt>
                   <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">Developer Tools in Canada</dt>
                   <dd className="order-1 text-5xl font-extrabold text-blue-600">#1</dd>
                 </div>
-                <div className="flex flex-col border-t border-b border-gray-100 dark:border-gray-900 p-6 text-center sm:border-0 sm:border-l sm:border-r">
+                <div className="flex flex-col p-6 text-center !border-l-0">
                   <dt className="order-3 mt-1 text-lg leading-6 font-medium text-gray-400">2021</dt>
                   <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">Developer Tools in the United States</dt>
                   <dd className="order-1 text-5xl font-extrabold text-cyan-600">#1</dd>
                 </div>
-                <div className="flex flex-col border-t border-b border-gray-100 dark:border-gray-900 p-6 text-center sm:border-0 sm:border-l sm:border-r">
+                <div className="flex flex-col p-6 text-center">
                   <dt className="order-3 mt-1 text-lg leading-6 font-medium text-gray-400">2021</dt>
                   <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">Apple Worldwide Developers Conference (WWDC)</dt>
                   <dd className="order-1 text-5xl font-extrabold text-orange-600 dark:text-orange-300">Winner</dd>
                 </div>
-                <div className="flex flex-col border-t border-gray-100 dark:border-gray-900 p-6 text-center sm:border-0 sm:border-l">
+                <div className="flex flex-col p-6 text-center">
                   <dt className="order-3 mt-1 text-lg leading-6 font-medium text-gray-400">Since 2020</dt>
-                  <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">Paid Apps in different categories Globally</dt>
+                  <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">Paid Apps in Different Categories Globally</dt>
                   <dd className="order-1 text-5xl font-extrabold text-teal-600">Top-100</dd>
                 </div>
               </dl>
