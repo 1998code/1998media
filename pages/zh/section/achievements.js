c1737a1bb9e175eef1b3f29eaa51a1b89d5bfc01
1998code <54872601+1998code@users.noreply.github.com>
2023-01-27 12:20:54 +0800

    Update text size

--- pages/zh/section/achievements.js
+++ pages/zh/section/achievements.js
@@ -16,41 +16,41 @@ export default function Achievements() {
         <div className="relative">
           <div className="relative max-w-7xl mx-auto">
             <div className="max-w-7xl mx-auto">
-            <dl className="rounded-lg bg-white dark:bg-black shadow-lg sm:grid sm:grid-cols-3 divide-gray-100 dark:divide-gray-900 divide-x-2 divide-y-2">
+            <dl className="rounded-lg bg-white dark:bg-black shadow-lg sm:grid sm:grid-cols-2 md:grid-cols-3">
                 <div className="flex flex-col p-6 text-center">
                   <dt className="order-3 mt-1 text-lg leading-6 font-medium text-gray-400">2022</dt>
                   <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">App Store 付費圖形與設計軟件</dt>
-                  <dd className="order-1 text-5xl font-extrabold text-pink-600">烏茲別克 第一</dd>
+                  <dd className="order-1 text-3xl font-extrabold text-sky-600">烏茲別克 第一</dd>
                 </div>
                 <div className="flex flex-col p-6 text-center">
                   <dt className="order-3 mt-1 text-lg leading-6 font-medium text-gray-400">2022</dt>
                   <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">App Store 付費開發者工具</dt>
-                  <dd className="order-1 text-5xl font-extrabold text-pink-600">科威特 第一</dd>
+                  <dd className="order-1 text-3xl font-extrabold text-pink-600">科威特 第一</dd>
                 </div>
                 <div className="flex flex-col p-6 text-center !border-t-0">
                   <dt className="order-3 mt-1 text-lg leading-6 font-medium text-gray-400">2022</dt>
                   <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">App Store 付費開發者工具</dt>
-                  <dd className="order-1 text-5xl font-extrabold text-green-600">台灣 第一</dd>
+                  <dd className="order-1 text-3xl font-extrabold text-green-600">台灣 第一</dd>
                 </div>
                 <div className="flex flex-col p-6 text-center !border-t-0">
                   <dt className="order-3 mt-1 text-lg leading-6 font-medium text-gray-400">2022</dt>
                   <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">App Store 付費開發者工具</dt>
-                  <dd className="order-1 text-5xl font-extrabold text-blue-600">加拿大 第一</dd>
+                  <dd className="order-1 text-3xl font-extrabold text-blue-600">加拿大 第一</dd>
                 </div>
                 <div className="flex flex-col p-6 text-center !border-l-0">
                   <dt className="order-3 mt-1 text-lg leading-6 font-medium text-gray-400">2021</dt>
                   <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">App Store 付費開發者工具</dt>
-                  <dd className="order-1 text-5xl font-extrabold text-cyan-600">美國 第一</dd>
+                  <dd className="order-1 text-3xl font-extrabold text-cyan-600">美國 第一</dd>
                 </div>
                 <div className="flex flex-col p-6 text-center">
                   <dt className="order-3 mt-1 text-lg leading-6 font-medium text-gray-400">2021</dt>
                   <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">Apple WWDC 全球開發者大會</dt>
-                  <dd className="order-1 text-5xl font-extrabold text-orange-600 dark:text-orange-300">獲獎</dd>
+                  <dd className="order-1 text-3xl font-extrabold text-orange-600 dark:text-orange-300">獲獎</dd>
                 </div>
                 <div className="flex flex-col p-6 text-center">
                   <dt className="order-3 mt-1 text-lg leading-6 font-medium text-gray-400">Since 2020</dt>
                   <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">全球不同類別的付費應用排行</dt>
-                  <dd className="order-1 text-5xl font-extrabold text-teal-600">前一百</dd>
+                  <dd className="order-1 text-3xl font-extrabold text-teal-600">前一百</dd>
                 </div>
               </dl>
             </div>
