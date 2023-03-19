7fc50551d77da26c45d325ca66b3575bf3b86e33
1998code <54872601+1998code@users.noreply.github.com>
2023-03-19 16:59:08 +0800

    Fix Safari

--- pages/[locale]/section/header.js
+++ pages/[locale]/section/header.js
@@ -11,7 +11,7 @@ export default function Header(props) {
             </span>
             <h1 data-aos="zoom-in" data-aos-delay="250" data-aos-duration="500" className="text-8xl font-bold mb-3 dark:text-white">{i18n("Hi")} <i className="fa-light fa-hand-wave text-orange-500"></i> {i18n("I'm")} <span className="text-orange-600 dark:text-orange-300 underline decoration-dotted decoration-2 underline-offset-8">{i18n("MING")}</span> !</h1>
             <h2 data-aos="zoom-in" data-aos-delay="750" data-aos-duration="500" className="text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-orange-500 to-orange-400">{i18n("Glad to see you here")} <i className="fa-regular fa-face-laugh-wink"></i></h2>
-            <div className="min-w-[50vw] h-[50vh] mx-auto">
+            <div className="h-[50vh]">
                 <spline-viewer url="/3ds/pig.splinecode" events-target="global"></spline-viewer>
             </div>
         </a>
