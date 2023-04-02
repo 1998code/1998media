f44588f7c793a4726ef8d7746cdecf0dc2e26892
1998code <54872601+1998code@users.noreply.github.com>
2023-04-02 11:44:48 +0800

    Update

--- pages/[locale]/section/credits.js
+++ pages/[locale]/section/credits.js
@@ -18,7 +18,9 @@ export default function Credits(props) {
         <img className="h-10 rounded-lg bg-white p-2 dark:hidden border border-black" src="https://docsearch.algolia.com/img/docsearch-logo.svg" alt="Algolia Docsearch" /> 
         <img className="h-10 rounded-lg hidden p-2 dark:bg-black dark:block border border-white" src="https://docsearch.algolia.com/img/docsearch-logo-white.svg" alt="Algolia Docsearch" /> 
         <img className="h-10 rounded-lg border border-black" src="/logos/GoogleResearch.png" alt="Google Research" />
-        <img className="h-10 rounded-lg bg-white p-2 border border-black" src="https://opensource.nyc3.cdn.digitaloceanspaces.com/attribution/assets/SVG/DO_Logo_icon_blue.svg" alt="DigitalOcean" />
+        <a href="https://www.digitalocean.com/?refcode=ce873177d9ab&utm_medium=opensource">
+          <img className="h-10 rounded-lg bg-white p-2 border border-black" src="https://opensource.nyc3.cdn.digitaloceanspaces.com/attribution/assets/SVG/DO_Logo_icon_blue.svg" alt="DigitalOcean" />
+        </a>
         <img className="h-10 rounded-lg border border-white" src="/logos/STACKPATH.png" alt="STACKPATH" />
         <img className="h-10 rounded-lg" src="/logos/fastly.png" alt="fastly" />
       </div>
--- pages/[locale]/section/faq.js
+++ pages/[locale]/section/faq.js
@@ -21,7 +21,7 @@ export default function Faq(props) {
           <i className="fas fa-pipe px-3"></i>
           <a href="https://vercel.com/?utm_source=1998code&utm_campaign=oss">Vercel<i className="ml-1 fa fa-external-link fa-sm"></i></a>
           <i className="fas fa-pipe px-3"></i>
-          <a href="https://digitialocean.com">DigitalOcean<i className="ml-1 fa fa-external-link fa-sm"></i></a>
+          <a href="https://www.digitalocean.com/?refcode=ce873177d9ab&utm_medium=opensource">DigitalOcean<i className="ml-1 fa fa-external-link fa-sm"></i></a>
         </span>
       ),
     },
