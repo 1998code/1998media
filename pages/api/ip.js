39c27a52eae1f5bdcd1f1f5330b2c60ea5073f24
1998code <54872601+1998code@users.noreply.github.com>
2024-04-23 17:46:32 +0800

    Allow language change

--- pages/api/ip.js
+++ pages/api/ip.js
@@ -1,24 +1,27 @@
 import NodeGeocoder from 'node-geocoder'
 
-let options = {
-    provider: 'openstreetmap',
-    language: 'en'
-  };
-  
-  let geoCoder = NodeGeocoder(options);
-
 export default async function (req, res) {
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
 
-    const latitude = '22.2842' || req.headers['x-vercel-ip-latitude']
-    const longitude = '114.1759' || req.headers['x-vercel-ip-longitude']
+    const latitude = req.headers['x-vercel-ip-latitude']
+    const longitude = req.headers['x-vercel-ip-longitude']
 
     let geo = null;
-    if(latitude && longitude) {
-        const res = await geoCoder.reverse({lat: latitude, lon: longitude});
+    if (latitude && longitude) {
+        const options = {
+            provider: 'openstreetmap',
+            language: req.headers['accept-language'].split(',')[0] || 'en',
+        };
+        const geoCoder = NodeGeocoder(options);
+        const res = await geoCoder.reverse({ lat: latitude, lon: longitude });
         geo = {
             city: res[0].city,
             country: res[0].country
+        }
+    } else {
+        geo = {
+            city: 'Unknown',
+            country: 'Unknown'
         }
     }
 
