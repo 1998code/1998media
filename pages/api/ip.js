91ea7ed02a2d8a9fcbcee25cc2b3016b4dadc455
1998code <54872601+1998code@users.noreply.github.com>
2024-04-23 16:21:45 +0800

    Disable IP

--- pages/api/ip.js
+++ pages/api/ip.js
@@ -1,13 +1,10 @@
-import geoip from 'geoip-lite'
+// import geoip from 'geoip-lite'
 
 export default function (req, res) {
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+    // const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+    const ip = null
 
-    let geo = geoip.lookup(ip)
-
-    if (!geo) {
-        geo = null
-    }
+    const geo = geoip.lookup(ip)
 
     const latitude = req.headers['x-vercel-ip-latitude']
     const longitude = req.headers['x-vercel-ip-longitude']
