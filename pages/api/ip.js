e0bdc2fbae505b7834a8e95a2f1f98308e429ea8
1998code <54872601+1998code@users.noreply.github.com>
2024-04-23 17:39:31 +0800

    Test GEO

--- pages/api/ip.js
+++ pages/api/ip.js
@@ -1,14 +1,25 @@
-// import geoip from 'geoip-lite'
+import NodeGeocoder from 'node-geocoder'
 
-export default function (req, res) {
-    // const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
-    const ip = null
+let options = {
+    provider: 'openstreetmap'
+  };
+  
+  let geoCoder = NodeGeocoder(options);
 
-    // const geo = geoip.lookup(ip)
-    const geo = null
+export default async function (req, res) {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
 
-    const latitude = req.headers['x-vercel-ip-latitude']
-    const longitude = req.headers['x-vercel-ip-longitude']
+    const latitude = '22.2842' || req.headers['x-vercel-ip-latitude']
+    const longitude = '114.1759' || req.headers['x-vercel-ip-longitude']
+
+    let geo = null;
+    if(latitude && longitude) {
+        const res = await geoCoder.reverse({lat: latitude, lon: longitude});
+        geo = {
+            city: res[0].city,
+            country: res[0].country
+        }
+    }
 
     res.status(200).json({ ip, geo, latitude, longitude })
 }
