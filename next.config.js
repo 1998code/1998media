a7600a7b765585b46b446115b6824014b7a9eec8
1998code <54872601+1998code@users.noreply.github.com>
2024-04-23 13:56:43 +0800

    Test

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.1998.media',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'cdn.1998.media',
+  //       port: '',
+  //       pathname: '/**',
+  //     },
+  //   ],
+  // },
 }
 
 module.exports = nextConfig
