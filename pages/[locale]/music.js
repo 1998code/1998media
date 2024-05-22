b698ee6981a34d6c20a6510bb16c74918e309a4b
1998code <54872601+1998code@users.noreply.github.com>
2024-05-22 16:16:44 +0800

    Update music.js

--- pages/[locale]/music.js
+++ pages/[locale]/music.js
@@ -70,7 +70,7 @@ export default function Music(props) {
               {music.map((item, index) => (
                 <div 
                   key={index} 
-                  className={`p-3 flex items-center justify-between ${index === music.findIndex((music) => music.id === currentPlaying.id) ? 'bg-red-100 dark:bg-red-800 text-red-800 dark:text-red-100' : 'dark:text-white'}`}
+                  className={`p-3 flex items-center justify-between ${index === music.findIndex((music) => music.id === currentPlaying.id) ? 'bg-red-100 dark:bg-red-600 text-red-800 dark:text-red-100' : 'dark:text-white'}`}
                 >
                   <div className="flex items-center gap-3">
                     <img src={item.attributes.artwork.url.replace("{w}","50").replace("{h}","50")} className="w-10 h-10 rounded-lg shadow-lg" />
