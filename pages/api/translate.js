5100093ed4c1e82c6d09033cd527416b22f86fd0
1998code <54872601+1998code@users.noreply.github.com>
2024-05-23 21:22:10 +0800

    Add Translate

---
+++ pages/api/translate.js
@@ -0,0 +1,12 @@
+import translatte from 'translatte';
+
+export default async function (req, res) {
+  const { text, from, to } = req.query;
+  const translation = await translatte(text, { from, to });
+
+  res.status(200).json({
+    input:text,
+    output:translation.text,
+    from, to
+  });
+}
