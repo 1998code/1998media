cb83d9ac57308f59334bd33881db639c2b366816
MING <54872601+1998code@users.noreply.github.com>
2026-02-18 19:36:23 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,6 @@ export default function Document(props) {
         <link
           rel="stylesheet"
           href="https://cdn.1998.media/css/fontawesome.css"
-          crossOrigin="anonymous"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto+Flex:wght@100..1000&display=swap"
